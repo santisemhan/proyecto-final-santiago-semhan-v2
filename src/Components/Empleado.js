65d0d1178b900086c1fffe6ad22bdb0b50512e24
Santiago <santisemhan2@gmail.com>
2020-12-04 10:01:18 -0300

    Cambiando el estilo de los empleados

--- src/Components/Empleado.js
+++ src/Components/Empleado.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { Button, Card, CardImg, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const moment = require("moment");
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 352,
+    margin: 10,
+  },
+  media: {
+    height: 352,
+  },
+});
 
 const Personaje = ({
   id,
@@ -14,26 +30,34 @@ const Personaje = ({
   created,
   goBack = false,
 }) => {
+  const classes = useStyles();
+
   return (
-    <Col xs={4} className="mb-2" key={id}>
-      <Card>
-        <CardImg src={image} />
-        <Card.Body>
-          <Card.Title>{name}</Card.Title>
-          <Card.Text>
-            {status === "Alive" ? "Vivo" : "Muerto"} - {gender} - {species}
-          </Card.Text>
-          <Card.Text>
-            Fecha de nacimiento: {moment(created).format("YYYY-MM-DD")}
-          </Card.Text>
-          <Link to={goBack ? "/personajes" : `/personaje/${id}`}>
-            <Button block>
-              {goBack ? "Volver" : "Informacion del Empleado"}
-            </Button>
-          </Link>
-        </Card.Body>
+    <>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={image}
+            title="imagen_empleado"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {name}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="textSecondary"
+              component="p"
+              display="block"
+            >
+              {status === "Alive" ? "Vivo" : "Muerto"} - {gender} - {species} -
+              {"\n"} Fecha de nacimiento: {moment(created).format("YYYY-MM-DD")}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
       </Card>
-    </Col>
+    </>
   );
 };
 
